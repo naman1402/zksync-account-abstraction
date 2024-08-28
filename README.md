1ff100eece96f1ea01383ad3ef2c794b97faf24a
naman mohnani <mohnaninaman@gmail.com>
2024-08-28 13:13:54 +0530

    working on multisig

--- README.md
+++ README.md
@@ -1,13 +1,2 @@
-# Sample Hardhat Project
-
-This project demonstrates a basic Hardhat use case. It comes with a sample contract, a test for that contract, and a Hardhat Ignition module that deploys that contract.
-
-Try running some of the following tasks:
-
-```shell
-npx hardhat help
-npx hardhat test
-REPORT_GAS=true npx hardhat test
-npx hardhat node
-npx hardhat ignition deploy ./ignition/modules/Lock.ts
-```
+IAccount - interface that allows contracts to manage accounts and validate operations in a more flexible way
+EIP1271 - defines how smart contracts validate signature, making it possible for contracts to act as wallets and verify the signatures of transactions
--- contracts/MultiSig.sol
+++ contracts/MultiSig.sol
@@ -5,15 +5,21 @@ import "@matterlabs/zksync-contracts/l2/system-contracts/interfaces/IAccount.sol
 import "@matterlabs/zksync-contracts/l2/system-contracts/libraries/TransactionHelper.sol";
 import "@openzeppelin/contracts/interfaces/IERC1271.sol";
 import "@matterlabs/zksync-contracts/l2/system-contracts/Constants.sol";
+
+// Used for signature validation
 import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
+// non-view methods of system contracts
 import "@matterlabs/zksync-contracts/l2/system-contracts/libraries/SystemContractsCaller.sol";
 
 error MultiSig__OnlyBootLoaderCanCall();
+error MultiSig__ExecutionFailed();
+error MultiSig__TransactionFailed();
 
 contract MultiSig is IAccount, IERC1271{
 
     using TransactionHelper for Transaction;
 
+    // account owners
     address public owner1;
     address public owner2;
     bytes4 constant EIP1271_SUCCESS_RETURN_VALUE = 0x1626ba7e;
@@ -27,6 +33,7 @@ contract MultiSig is IAccount, IERC1271{
         if(msg.sender != BOOTLOADER_FORMAL_ADDRESS) {
             revert MultiSig__OnlyBootLoaderCanCall();
         }
+        // continue execution if called from bootloader
         _;
     }
 
@@ -38,6 +45,10 @@ contract MultiSig is IAccount, IERC1271{
         magic = _validateTransaction(_suggestedSignedHash, _transaction);
     }
 
+    /// Using system contract to call nonce holder contract to increment the nonce value
+    /// if suggestedHash is not provided(btyes32(0)), then generate it using Transaction functions
+    /// Check if account contract has enough balance compared to requiredBalance()
+    /// Calling EIP1271 function to check if the signature is valid or not
     function _validateTransaction( bytes32 _suggestedSignedHash, Transaction calldata _transaction) internal returns (bytes4 magic) {
         SystemContractsCaller.systemCallWithPropagatedRevert(uint32(gasleft()), address(NONCE_HOLDER_SYSTEM_CONTRACT), 0, abi.encodeCall(INonceHolder.incrementMinNonceIfEquals, (_transaction.nonce)));
         bytes32 txHash;
@@ -60,7 +71,7 @@ contract MultiSig is IAccount, IERC1271{
         bytes32 /*_txHash*/,
         bytes32 /*_suggestedSignedHash*/,
         Transaction calldata _transaction
-    ) external payable{
+    ) external payable override onlyBootLoader{
         _executeTransaction(_transaction);
     }
 
@@ -76,24 +87,32 @@ contract MultiSig is IAccount, IERC1271{
             assembly {
                 success := call(gas(), to, value, add(data, 0x20), mload(data), 0, 0)
             }
-            require(success);
+            if(!success) {
+                revert MultiSig__ExecutionFailed();
+            }
         }
     }
 
+    // To execute from outside, first we need to validate and then execute
     function executeTransactionFromOutside(Transaction calldata _transaction) external payable {
         _validateTransaction(bytes32(0), _transaction);
         _executeTransaction(_transaction);
     }
 
+    // Pays required fees to the bootloader
+    // Bootloader allows for processing not just one transaction at a time but an entire batch of transactions as a single large operation
     function payForTransaction(
         bytes32 /*_txHash*/,
         bytes32 /*_suggestedSignedHash*/,
         Transaction calldata _transaction
-    ) external payable {
+    ) external payable override onlyBootLoader() {
         bool success = TransactionHelper.payToTheBootloader(_transaction);
-        require(success);
+        if(!success) {
+            revert MultiSig__TransactionFailed();
+        }
     }
 
+    // Paymaster sponsors gas fees for the transaction
     function prepareForPaymaster(
         bytes32 /*_txHash*/,
         bytes32 /*_possibleSignedHash*/,
@@ -102,7 +121,80 @@ contract MultiSig is IAccount, IERC1271{
         TransactionHelper.processPaymasterInput(_transaction);
     }
 
-    function isValidSignature(bytes32 hash, bytes memory signature) public view returns (bytes4 magicValue) {}
+    // Overrider from EIP1271, 
+    function isValidSignature(bytes32 hash, bytes memory signature) public view override returns (bytes4 magicValue) {
+        magicValue = EIP1271_SUCCESS_RETURN_VALUE;
+        if(signature.length != 130) {
+            signature = new bytes(130);
+            signature[64] = bytes1(uint8(27));
+            signature[129] = bytes1(uint8(27));
+
+        }
+
+        (bytes memory signature1, bytes memory signature2) = extractSignature(signature);
+        if(!checkValidECDSASignture(signature1) || !checkValidECDSASignture(signature2)) {
+            magicValue = bytes4(0);
+        }
+
+        address recoveredAddr1 = ECDSA.recover(hash, signature1);
+        address recoveredAddr2 = ECDSA.recover(hash, signature2);
+        if(recoveredAddr1 != owner1 || recoveredAddr2 != owner2) {
+            magicValue = bytes4( 0);
+        }
+    }
+
+    function checkValidECDSASignture(bytes memory _signature) internal pure returns (bool) {
+        if(_signature.length != 65) {
+            return false;
+        }
+
+        uint8 v;
+        bytes32 r;
+        bytes32 s;
+
+        assembly {
+            r := mload(add(_signature, 0x20))
+            s := mload(add(_signature, 0x40))
+            v := and(mload(add(_signature, 0x41)), 0xff)
+        }
+
+        if(v != 27 && v != 28) {
+            return false;
+        }
+
+
+        if(uint256(s) > 0x7FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF5D576E7357A4501DDFE92F46681B20A0) {
+            return false;
+        }
+        return true;
+    }
+
+    function extractSignature(bytes memory _fullSignature) internal pure returns (bytes memory signature1, bytes memory signature2) {
+        require(_fullSignature.length == 130);
+        signature1 = new bytes(65);
+        signature2 = new bytes(65);
+
+
+        assembly {
+            let r := mload(add(_fullSignature, 0x20))
+            let s := mload(add(_fullSignature, 0x40))
+            let v := and(mload(add(_fullSignature, 0x41)), 0xff)
+
+            mstore(add(signature1, 0x20), r)
+            mstore(add(signature1, 0x40), s)
+            mstore8(add(signature1, 0x60), v)
+        }
+
+        assembly {
+            let r := mload(add(_fullSignature, 0x61))
+            let s := mload(add(_fullSignature, 0x81))
+            let v := and(mload(add(_fullSignature, 0x82)), 0xff)
+
+            mstore(add(signature2, 0x20), r)
+            mstore(add(signature2, 0x40), s)
+            mstore8(add(signature2, 0x60), v)
+        }
+    }
 
     fallback() external {
         assert(msg.sender != BOOTLOADER_FORMAL_ADDRESS);
